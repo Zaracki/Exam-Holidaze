14824dc410b832c7b1d7b689f38ce034534aeb7c
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-09-22 17:02:45 +0200

    New loader

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -54,14 +54,31 @@ const Profile = () => {
     }));
   };
 
-  if (isLoadingBookings || isLoadingVenues) return <p>Loading...</p>;
+  if (isLoadingBookings || isLoadingVenues) {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-black">
+        <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+          <svg className="text-gray-300 animate-spin" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" width="48" height="48">
+            <path
+              d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+              stroke="currentColor" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round"></path>
+            <path
+              d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+              stroke="currentColor" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" className="text-gray-900">
+            </path>
+          </svg>
+        </div>
+      </div>
+    );
+  }
+
   if (hasErrorBookings || hasErrorVenues) return <p>Error loading profile.</p>;
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-grey p-4">
       <div className="max-w-[632px] w-full mb-8">
         <h1 className="text-3xl font-medium mb-6">Profile</h1>
-        <div className="bg-white p-6">
+        <div className="bg-white border p-6 mb-5 bg-grey">
           <div className="flex items-center mb-6">
             {bookingsData?.avatar ? (
               <img
@@ -90,9 +107,11 @@ const Profile = () => {
         </div>
         {userProfile?.venueManager && (
           <>
-            <Link to="/CreateVenue">
-              <PrimaryButton>Create Venue</PrimaryButton>
-            </Link>
+            <div className="flex justify-center">
+              <Link to="/CreateVenue">
+                <PrimaryButton>Create Venue</PrimaryButton>
+              </Link>
+            </div>
             <h2 className="text-3xl font-medium mb-4 mt-6">My Venues</h2>
             <hr className="mt-3 mb-6 border-gray-400" />
             <div className="space-y-4">
