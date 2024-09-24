65d89a77004e369136b6fd74b562a3d3246dad30
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-09-24 14:13:32 +0200

    Update Footer.jsx

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer>
-      <div className="container mx-auto text-center">
+    <footer className="w-full bg-zinc-900 text-center text-white">
+      <div className="w-full mx-0 py-4">
         <p>&copy; 2024 Holidaze. All rights reserved.</p>
       </div>
     </footer>
